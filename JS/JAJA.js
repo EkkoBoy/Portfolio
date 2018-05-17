7af7a3e120d9f5284aad209383f65ddb34b44dc7
Almani Nadia <Almani Nadia@DESKTOP-O464Q3I>
2018-05-17 14:44:13 +0200

    premier

---
+++ JS/JAJA.js
@@ -0,0 +1,8 @@
+
+
+
+var lien1 = document.querySelector("#lien1");
+lien1.onmouseover = function () {
+    this.href = "https://www.beatsbydre.com/fr";
+    this.innerHTML = "Beats By Dre";
+};
---
+++ Page1.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link href="CSS/Style.css" rel="stylesheet" type="text/css"/>
+
+</head>
+<body>
+    <nav class=" fixed navbar navbar-dark bg-dark" id="barnav">
+        <a class="navbar-brand" href="#">Manuel Garrido</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Parcours</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Projets
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Start</a>
+                        <a class="dropdown-item" href="#">Evolution</a>
+                        <a class="dropdown-item" href="#">Cas H</a>
+                        <div class="dropdown-divider"></div>
+
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">CV</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Recherche" aria-label="Recherche">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Recherche</button>
+            </form>
+    </nav>
+
+    <header class="head text text-color d-flex">
+        <div id="centerimage">
+            <img src="Images/Moi.jpg" class="photomoi" alt=""/>
+            <h1>A SAVOIR</h1>
+            <hr>
+        </div>
+
+    </header>
+    <section class="color">
+
+    </section>
+    <section class="conteneur" id="projets">
+        <img src="Images/2.png" class="tailleimage" alt=""/>
+        <img src="Images/3.png"class="tailleimage" alt=""/>
+        <img src="Images/1.png" class="tailleimage" alt=""/>
+    </section>
+    <section id="CV">
+    </section>
+    <section id="pieddepage">
+        
+    </section>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="JS/bootstrap.js" type="text/javascript"></script>
+<script src="JS/JAJA.js" type="text/javascript"></script>
+</body>
+</html>
